44ae6b4f4a7a6cb1d5a98d4b7cf57ffaf20ca349
NiKuDev <niek_kuijken@hotmail.com>
2019-11-12 14:08:33 +0100

    - Added first controller to get eulerchallenges by id
    Signed-off-by: NiKuDev <niek_kuijken@hotmail.com>

---
+++ ProjectEulerAPI/Controllers/EulerController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjectEulerAPI.Services;
+
+namespace ProjectEulerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EulerController : ControllerBase
+    {
+        private readonly EulerService _eulerService;
+
+        public EulerController()
+        {
+            this._eulerService = new EulerService();
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "EulerController";
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public int Get(int id)
+        {
+            if (id == 1)
+                return this._eulerService._1();
+            else if (id == 2)
+                return this._eulerService._2();
+
+            return 00000;
+        }
+    }
+}
